fcdcf67a2543cc4b2424377c3e93d147e29e5a94
Kasia Pikulicka <kasiapikulicka@MacBook-Air-Kasia.local>
2025-02-18 14:20:50 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,64 @@
+import "./App.css";
+import "./index.css";
+import { useState } from "react";
+import { Play } from "lucide-react";
+
+function SoundButton() {
+  const [images, setImages] = useState([]);
+
+  const playSound = () => {
+    const audio = new Audio("/sound.mp3");
+    audio.currentTime = 0;
+    audio.play().catch((error) => console.error("Błąd odtwarzania:", error));
+
+    const newImages = [];
+    for (let i = 0; i < 25; i++) {
+      const randomX = Math.random() * 300 - 150;
+      const randomY = Math.random() * -300 - 50;
+      const randomRotate = Math.random() * 360;
+
+      newImages.push({
+        id: i,
+        x: randomX,
+        y: randomY,
+        rotate: randomRotate,
+      });
+    }
+
+    setImages(newImages);
+    setTimeout(() => setImages([]), 4000);
+  };
+
+  return (
+    <div className="container">
+      <button onClick={playSound} className="sound-button">
+        <Play size={72} />
+      </button>
+      <div className="images-container">
+        {images.map((img) => (
+          <img
+            key={img.id}
+            src="/cat.png"
+            alt="Obrazek"
+            className="image-fall"
+            style={{
+              "--x": `${img.x}px`,
+              "--y": `${img.y}px`,
+              "--rotate": `${img.rotate}deg`,
+            }}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <SoundButton />
+    </div>
+  );
+}
+
+export default App;
